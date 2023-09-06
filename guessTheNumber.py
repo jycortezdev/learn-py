82ecc1a25c17b7e9b7047c4e6b9b255085c4ac5b
jycortezdev <143844565+jycortezdev@users.noreply.github.com>
2023-09-05 19:10:08 -0500

    Learn py (#6)
    * hello world
    
    * add .gitignore file
    
    * practiced loops!
    
    * code cleanup
    
    * rand ints
    
    * practicing using import statements
    
    * added a short program that makes the user guess the randomly generated integer.
    
    * code cleanup.
    
    * added the rock, paper, scissors program
    
    * added collatzSequence.py

---
+++ guessTheNumber.py
@@ -0,0 +1,22 @@
+# This is a guess the numbner game.
+import random
+
+secretNumber = random.randint(1,20) #generates a random integer from 1 through 20
+print('I am thinking of a number between 1 and 20.')
+
+# Ask the player to guess 6 times.
+for guessesTaken in range(1,7):
+    print('Take a guess.')
+    guess = int(input()) #cast the input as an integer.
+
+    if guess < secretNumber:
+        print('Your guess is too low.')
+    elif guess > secretNumber:
+        print('Your guess is too high.')
+    else:
+        break #the program exits because the user guessed the correct number.
+
+if guess == secretNumber:
+    print('Good job! You guessed my number in  ' + str(guessesTaken) + ' guesses!')
+else:
+    print('Nope. the number I was thinking of was ' + str(secretNumber))
---
+++ learnPyInputs.py
@@ -0,0 +1,5 @@
+#trying the import function in Python
+import random
+
+for i in range(5):
+    print(random.randint(1,10)) # the randint function from the random module generates random integers.
