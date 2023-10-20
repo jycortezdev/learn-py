1e2d2c747d718c36d809d1b9c365f72bf65c7edf
Joshan Cortez <joshan.cortez@gmail.com>
2023-10-20 17:44:16 -0500

    .

---
+++ exploringLists.py
@@ -0,0 +1,23 @@
+# Writing my first list!
+
+# myList = ['cat', 'bat', 'rat', 'elephant']
+# pets =[['cat','dog'], 'cars']
+# cat = ['fat', 'gray', 'loud']
+# supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
+
+# for index, item in enumerate(supplies):
+#     print('Index ' + str(index) + ' in supplies is: ' + item)
+
+# trying out random.choice() and random.shuffle()
+import random
+
+pets = ['Dog', 'Cat', 'Moose']
+# print(random.choice(pets))
+# print(random.choice(pets))
+# print(random.choice(pets))
+
+people = ['Alice', 'Bob', 'Carol', 'David']
+print(people)
+shuffled = []
+shuffled = random.shuffle(people)
+print(shuffled)
